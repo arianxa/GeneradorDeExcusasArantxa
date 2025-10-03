b92945d78e38e77588c3ab69aae14470574675b0
arianxa <44567127+arianxa@users.noreply.github.com>
2025-10-03 08:01:43 +0000

    s

--- src/script.js
+++ src/script.js
@@ -1,25 +1,4 @@
 
-
-
-/*Usando JavaScript, crea una función que genere y devuelva una excusa aleatoria con la siguiente estructura:
-let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
-let action = ['ate', 'peed', 'crushed', 'broke'];
-let what = ['my homework', 'my phone', 'the car'];
-let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
-Para crear una excusa consistente, tienes que concatenar un elemento de cada array o arreglo en el orden correcto.
-Usa la función onload y establece la excusa en el innerHTML del elemento HTML #excuse.
-Tecnologías
-HTML, CSS3, JavaScript, Vite.
-
-Fundamentos
-Este ejercicio cubre los siguientes fundamentos:
-
-Utilizar archivos JavaScript externos en tu proyecto.
-Cómo trabajar con arreglos/listas (arrays).
-Generar números al azar.
-Concatenar strings (cadenas).
-Utilizar funciones (al menos un poco).
-Trabajar con eventos (al menos un poco).*/
 let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
 let action = ['ate', 'peed', 'crushed', 'broke'];
 let what = ['my homework', 'my phone', 'the car'];
@@ -34,6 +13,7 @@ let palabraAleatoriaWhat = what[indiceAleatorioWhat];
 let indiceAleatorioWhen = Math.floor(Math.random() * when.length);
 let palabraAleatoriaWhen = when[indiceAleatorioWhen];
 let total=palabraAleatoriaWho+"&nbsp;"+palabraAleatoriaAction+"&nbsp;"+palabraAleatoriaWhat+"&nbsp;"+ palabraAleatoriaWhen;
+//tambien se puede usar "" en ves de "&nbsp;" pero como nos enseñasteis eso pues lo uso...
 
 let final=document.getElementById('excuse');
 
